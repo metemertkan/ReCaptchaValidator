e157b07a18c7b1c0c532ebe0d128d157bb1f9521
metemertkan <metemertkan@gmail.com>
2017-02-21 00:02:39 +0200

    Mvc project for action filter

---
+++ ValidateWithActionFilter/ActionFilters/ValidateReCaptchaAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using GoogleReCaptchaV2Validator;
+
+namespace ValidateWithActionFilter.ActionFilters
+{
+    public class ValidateReCaptchaAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var reCaptchaPrivateKey = ConfigurationManager.AppSettings["reCaptcha_privateKey"];
+            var reCaptchaResponse = filterContext.HttpContext.Request.Form["g-recaptcha-response"];
+
+            var validator = new ReCaptchaValidator(reCaptchaPrivateKey);
+            var validatorResponse = validator.Validate(reCaptchaResponse);
+
+            if (!validatorResponse.Success)
+            {
+                filterContext.Controller.ViewData.ModelState.AddModelError("ErrorCodes", string.Join(",", validatorResponse.ErrorCodes));
+            }            
+        }
+    }
+}
---
+++ ValidateWithActionFilter/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidateWithActionFilter.ActionFilters;
+
+namespace ValidateWithActionFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateReCaptcha]
+        public ActionResult TestRecaptcha()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+    }
+}
